2a95147aa0dbd45ec797e9e3d055ba5ed1200f41
Chengyu Song <csong@cs.ucr.edu>
2023-04-23 20:59:28 -0700

    simple AST parsing

--- driver/aflpp/symsan.cpp
+++ driver/aflpp/symsan.cpp
@@ -5,7 +5,9 @@
 */
 
 #include "dfsan/dfsan.h"
-#include "rgd.pb.h"
+
+#include "ast.h"
+#include "task.h"
 
 extern "C" {
 #include "afl-fuzz.h"
@@ -54,27 +56,274 @@ typedef struct my_mutator {
   char **argv;
   int out_fd;
   int shm_id;
-  dfsan_label_info *label_info;
 } my_mutator_t;
 
 // FIXME: a temporary way to find out input that has been fuzzed before
 static std::unordered_set<u32> __fuzzed_inputs;
 
-// global caches
-static std::unordered_map<dfsan_label, std::shared_ptr<u8>> memcmp_cache;
+// FIXME: find another way to make the union table hash work
+static dfsan_label_info *__dfsan_label_info;
 
-static inline dfsan_label_info *get_label_info(dfsan_label label, dfsan_label_info *label_info) {
-  return &label_info[label];
+dfsan_label_info* __dfsan::get_label_info(dfsan_label label) {
+  return &__dfsan_label_info[label];
 }
 
+static const std::unordered_map<unsigned, std::pair<unsigned, const char*> > OP_MAP {
+  {__dfsan::Extract, {rgd::Extract, "extract"}},
+  {__dfsan::Trunc,   {rgd::Extract, "extract"}},
+  {__dfsan::Concat,  {rgd::Concat, "concat"}},
+  {__dfsan::ZExt,    {rgd::ZExt, "zext"}},
+  {__dfsan::SExt,    {rgd::SExt, "sext"}},
+  {__dfsan::Add,     {rgd::Add, "add"}},
+  {__dfsan::Sub,     {rgd::Sub, "sub"}},
+  {__dfsan::UDiv,    {rgd::UDiv, "udiv"}},
+  {__dfsan::SDiv,    {rgd::SDiv, "sdiv"}},
+  {__dfsan::SRem,    {rgd::SRem, "srem"}},
+  {__dfsan::Shl,     {rgd::Shl, "shl"}},
+  {__dfsan::LShr,    {rgd::LShr, "lshr"}},
+  {__dfsan::AShr,    {rgd::AShr, "ashr"}},
+  {__dfsan::And,     {rgd::And, "and"}},
+  {__dfsan::Or,      {rgd::Or, "or"}},
+  {__dfsan::Xor,     {rgd::Xor, "xor"}},
+  // relational comparisons
+#define RELATIONAL_ICMP(cmp) (__dfsan::ICmp | (cmp << 8)) 
+  {RELATIONAL_ICMP(__dfsan::bveq),  {rgd::Equal, "equal"}},
+  {RELATIONAL_ICMP(__dfsan::bvneq), {rgd::Distinct, "distinct"}},
+  {RELATIONAL_ICMP(__dfsan::bvugt), {rgd::Ugt, "ugt"}},
+  {RELATIONAL_ICMP(__dfsan::bvuge), {rgd::Uge, "uge"}},
+  {RELATIONAL_ICMP(__dfsan::bvult), {rgd::Ult, "ult"}},
+  {RELATIONAL_ICMP(__dfsan::bvule), {rgd::Ule, "ule"}},
+  {RELATIONAL_ICMP(__dfsan::bvsgt), {rgd::Sgt, "sgt"}},
+  {RELATIONAL_ICMP(__dfsan::bvsge), {rgd::Sge, "sge"}},
+  {RELATIONAL_ICMP(__dfsan::bvslt), {rgd::Slt, "slt"}},
+  {RELATIONAL_ICMP(__dfsan::bvsle), {rgd::Sle, "sle"}},
+#undef RELATIONAL_ICMP
+};
+
+typedef std::shared_ptr<rgd::AstNode> SymExpr;
+
+// global caches
+static std::unordered_map<dfsan_label, SymExpr> expr_cache;
+static std::unordered_map<dfsan_label, std::unordered_set<size_t> > input_dep_cache;
+static std::unordered_map<dfsan_label, std::shared_ptr<u8>> memcmp_cache;
+
 static void clear_global_caches() {
+  expr_cache.clear();
+  input_dep_cache.clear();
   memcmp_cache.clear();
 }
 
-static void handle_cond(pipe_msg *msg, dfsan_label_info *label_info) {
+static uint32_t map_arg(const u8 *buf, size_t offset, uint32_t length,
+                        std::shared_ptr<rgd::Constraint> constraint) {
+  uint32_t hash = 0;
+  for (uint32_t i = 0; i < length; ++i, ++offset) {
+    u8 val = buf[offset];
+    uint32_t arg_index = 0;
+    auto itr = constraint->local_map.find(offset);
+    if (itr == constraint->local_map.end()) {
+      arg_index = (uint32_t)constraint->input_args.size();
+      constraint->inputs.insert({offset, val});
+      constraint->local_map[offset] = arg_index;
+      constraint->input_args.push_back(std::make_pair(true, 0)); // 0 is to be filled in the aggragation
+    } else {
+      arg_index = itr->second;
+    }
+    if (i == 0) {
+      constraint->shapes[offset] = length;
+      hash = rgd::xxhash(length * 8, rgd::Read, arg_index);
+    } else {
+      constraint->shapes[offset] = 0;
+    }
+  }
+  return hash;
 }
 
-static void handle_gep(gep_msg *gmsg, pipe_msg *msg, dfsan_label_info *label_info) {
+// this combines both AST construction and arg mapping
+static bool do_uta_rel(dfsan_label label, rgd::AstNode *ret,
+                       const u8 *buf, size_t buf_size,
+                       std::shared_ptr<rgd::Constraint> constraint,
+                       std::unordered_set<dfsan_label> &visited) {
+
+  if (label < CONST_OFFSET || label == kInitializingLabel) {
+    WARNF("invalid label: %d\n", label);
+    return false;
+  }
+
+  dfsan_label_info *info = get_label_info(label);
+  DEBUGF("%u = (l1:%u, l2:%u, op:%u, size:%u, op1:%lu, op2:%lu)\n",
+         label, info->l1, info->l2, info->op, info->size, info->op1.i, info->op2.i);
+
+  // we can't really reuse AST nodes across constraints,
+  // but we still need to avoid duplicate nodes within a constraint
+  if (visited.count(label)) {
+    // if a node has been visited, just record its label without expanding
+    ret->set_label(label);
+    ret->set_bits(info->size);
+    return true;
+  }
+
+  // terminal node
+  if (info->op == 0) {
+    // input
+    ret->set_kind(rgd::Read);
+    ret->set_bits(8);
+    ret->set_label(label);
+    uint64_t offset = info->op1.i;
+    assert(offset < buf_size);
+    // map arg
+    uint32_t hash = map_arg(buf, offset, 1, constraint);
+    ret->set_hash(hash);
+#if NEED_OFFLINE
+    std::string val;
+    rgd::buf_to_hex_string(&buf[offset], 1, val);
+    ret->set_value(std::move(val));
+    ret->set_index(offset);
+    ret->set_name("read");
+#endif
+    return true;
+  } else if (info->op == __dfsan::Load) {
+    ret->set_kind(rgd::Read);
+    ret->set_bits(info->l2 * 8);
+    ret->set_label(label);
+    uint64_t offset = get_label_info(info->l1)->op1.i;
+    assert(offset + info->l2 <= buf_size);
+    // map arg
+    uint32_t hash = map_arg(buf, offset, info->l2, constraint);
+    ret->set_hash(hash);
+#if NEED_OFFLINE
+    std::string val;
+    rgd::buf_to_hex_string(&buf[offset], info->l2, val);
+    ret->set_value(std::move(std::to_string(val)));
+    ret->set_index(offset);
+    ret->set_name("read");
+#endif
+    return true;
+  }
+
+  // common ops, make sure no special ops
+  auto op_itr = OP_MAP.find(info->op);
+  if (op_itr == OP_MAP.end()) {
+    WARNF("invalid op: %u\n", info->op);
+    return false;
+  }
+  ret->set_kind(op_itr->second.first);
+  ret->set_bits(info->size);
+  ret->set_label(label);
+#if NEED_OFFLINE
+  ret->set_name(op_itr->second.second);
+#endif
+
+  // now we visit the children
+  rgd::AstNode *left = ret->add_children();
+  if (info->l1 >= CONST_OFFSET) {
+    if (!do_uta_rel(info->l1, left, buf, buf_size, constraint, visited)) {
+      return false;
+    }
+    visited.insert(info->l1);
+  } else {
+    // constant
+    left->set_kind(rgd::Constant);
+    left->set_label(0);
+    uint32_t size = info->size;
+    // size of concat the sum of the two operands
+    // to get the size of the constant, we need to subtract the size
+    // of the other operand
+    if (info->op == __dfsan::Concat) {
+      assert(info->l2 >= CONST_OFFSET);
+      size -= get_label_info(info->l2)->size;
+    }
+    left->set_bits(size);
+    // map args
+    uint32_t arg_index = (uint32_t)constraint->input_args.size();
+    constraint->input_args.push_back(std::make_pair(false, info->op1.i));
+    constraint->const_num += 1;
+    uint32_t hash = rgd::xxhash(size, rgd::Constant, arg_index);
+    left->set_hash(hash);
+#if NEED_OFFLINE
+    left->set_value(std::to_string(info->op1.i));
+    left->set_index(arg_index);
+    left->set_name("constant");
+#endif
+  }
+  
+  // unary ops
+  if (info->op == __dfsan::ZExt || info->op == __dfsan::SExt ||
+      info->op == __dfsan::Extract || info->op == __dfsan::Trunc) {
+    uint32_t hash = rgd::xxhash(info->size, ret->kind(), left->hash());
+    ret->set_hash(hash);
+    uint64_t offset = info->op == __dfsan::Extract ? info->op2.i : 0;
+    ret->set_index(offset);
+    return true;
+  }
+
+  rgd::AstNode *right = ret->add_children();
+  if (info->l2 >= CONST_OFFSET) {
+    if (!do_uta_rel(info->l2, right, buf, buf_size, constraint, visited)) {
+      return false;
+    }
+    visited.insert(info->l2);
+  } else {
+    // constant
+    right->set_kind(rgd::Constant);
+    right->set_label(0);
+    uint32_t size = info->size;
+    // size of concat the sum of the two operands
+    // to get the size of the constant, we need to subtract the size
+    // of the other operand
+    if (info->op == __dfsan::Concat) {
+      assert(info->l1 >= CONST_OFFSET);
+      size -= get_label_info(info->l1)->size;
+    }
+    right->set_bits(size);
+    // map args
+    uint32_t arg_index = (uint32_t)constraint->input_args.size();
+    constraint->input_args.push_back(std::make_pair(false, info->op2.i));
+    constraint->const_num += 1;
+    uint32_t hash = rgd::xxhash(size, rgd::Constant, arg_index);
+    right->set_hash(hash);
+#if NEED_OFFLINE
+    right->set_value(std::to_string(info->op1.i));
+    right->set_index(arg_index);
+    right->set_name("constant");
+#endif
+  }
+
+  uint32_t hash = rgd::xxhash(left->hash(), ret->kind(), right->hash());
+  ret->set_hash(hash);
+
+  return ret;
+}
+
+static bool union_to_ast(bool r, dfsan_label label, dfsan_label_info *label_info,
+                         const u8 *buf, size_t buf_size,
+                         std::vector<SymExpr> &exprs) {
+
+  // check if the label is already in the cache
+  if (expr_cache.find(label) != expr_cache.end()) {
+    exprs.push_back(expr_cache[label]);
+    return true;
+  }
+
+  std::unordered_set<dfsan_label> visited;
+  std::shared_ptr<rgd::Constraint> constraint = std::make_shared<rgd::Constraint>();
+  rgd::AstNode *root = new rgd::AstNode();
+  do_uta_rel(label, root, buf, buf_size, constraint, visited);
+  return true;
+}
+
+static void handle_cond(pipe_msg &msg, const u8 *buf, size_t buf_size) {
+  if (unlikely(msg.label == 0)) {
+    return;
+  }
+
+  // parse the uniont table AST to protobuf ASTs
+  // each protobuf AST is a single relational expression
+  std::vector<SymExpr> exprs;
+  union_to_ast(msg.result == 0, msg.label, __dfsan_label_info,
+               buf, buf_size, exprs);
+}
+
+static void handle_gep(gep_msg &gmsg, pipe_msg &msg) {
 }
 
 /// no splice input
@@ -140,8 +389,8 @@ extern "C" my_mutator_t *afl_custom_init(afl_state *afl, unsigned int seed) {
     FATAL("Failed to get shmid: %s\n", strerror(errno));
   }
 
-  data->label_info = (dfsan_label_info *)shmat(data->shm_id, NULL, SHM_RDONLY);
-  if (data->label_info == (void *)-1) {
+  __dfsan_label_info = (dfsan_label_info *)shmat(data->shm_id, NULL, SHM_RDONLY);
+  if (__dfsan_label_info == (void *)-1) {
     FATAL("Failed to map shm(%d): %s\n", data->shm_id, strerror(errno));
   }
 
@@ -153,27 +402,12 @@ extern "C" my_mutator_t *afl_custom_init(afl_state *afl, unsigned int seed) {
 extern "C" void afl_custom_deinit(my_mutator_t *data) {
   close(data->out_fd);
   // unlink(data->out_file);
-  shmdt(data->label_info);
+  shmdt(__dfsan_label_info);
   ck_free(data->argv);
 }
 
-/// @brief the trace stage for symsan
-/// @param data the custom mutator state
-/// @param buf input buffer
-/// @param buf_size 
-/// @return the number of solving tasks
-extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
-                                     size_t buf_size) {
-
-  // check the input id to see if it's been run before
-  // we don't use the afl_custom_queue_new_entry() because we may not
-  // want to solve all the tasks
-  u32 input_id = data->afl->queue_cur->id;
-  if (__fuzzed_inputs.find(input_id) != __fuzzed_inputs.end()) {
-    return 0;
-  }
-  __fuzzed_inputs.insert(input_id);
-
+static int spawn_symsan_child(my_mutator_t *data, const u8 *buf, size_t buf_size,
+                              int pipefds[2]) {
   // setup argv in case of initialized
   if (unlikely(!data->argv)) {
     int argc = 0;
@@ -200,13 +434,6 @@ extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
     return 0;
   }
 
-  // create pipe for communication
-  int pipefds[2];
-  if (pipe(pipefds) != 0) {
-    WARNF("Failed to create pipe fds: %s\n", strerror(errno));
-    return 0;
-  }
-
   // setup the env vars for SYMSAN
   const char *taint_file = data->afl->fsrv.use_stdin ? "stdin" : data->out_file;
   char *options = alloc_printf("taint_file=%s:shm_id=%d:pipe_fd=%d:debug=%d",
@@ -214,11 +441,6 @@ extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
   DEBUGF("TAINT_OPTIONS=%s\n", options);
   
   int pid = fork();
-  if (pid < 0) {
-    WARNF("Failed to fork: %s\n", strerror(errno));
-    return 0;
-  }
-
   if (pid == 0) {
     close(pipefds[0]); // close the read fd
     setenv("TAINT_OPTIONS", (char*)options, 1);
@@ -235,10 +457,50 @@ extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
     execv(data->symsan_bin, data->argv);
     DEBUGF("Failed to execv: %s", data->symsan_bin);
     exit(-1);
+  } if (pid < 0) {
+    WARNF("Failed to fork: %s\n", strerror(errno));
   }
 
+  // free options
+  ck_free(options);
+
+  return pid;
+
+}
+
+/// @brief the trace stage for symsan
+/// @param data the custom mutator state
+/// @param buf input buffer
+/// @param buf_size 
+/// @return the number of solving tasks
+extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
+                                     size_t buf_size) {
+
+  // check the input id to see if it's been run before
+  // we don't use the afl_custom_queue_new_entry() because we may not
+  // want to solve all the tasks
+  u32 input_id = data->afl->queue_cur->id;
+  if (__fuzzed_inputs.find(input_id) != __fuzzed_inputs.end()) {
+    return 0;
+  }
+  __fuzzed_inputs.insert(input_id);
+
+  // create pipe for communication
+  int pipefds[2];
+  if (pipe(pipefds) != 0) {
+    WARNF("Failed to create pipe fds: %s\n", strerror(errno));
+    return 0;
+  }
+
+  // spawn the symsan child
+  int pid = spawn_symsan_child(data, buf, buf_size, pipefds);
   close(pipefds[1]); // close the write fd
 
+  if (pid < 0) {
+    close(pipefds[0]);
+    return 0;
+  }
+ 
   pipe_msg msg;
   gep_msg gmsg;
   dfsan_label_info *info;
@@ -255,7 +517,7 @@ extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
     switch (msg.msg_type) {
       // conditional branch
       case cond_type:
-        handle_cond(&msg, data->label_info);
+        handle_cond(msg, buf, buf_size);
         break;
       case gep_type:
         if (read(pipefds[0], &gmsg, sizeof(gmsg)) != sizeof(gmsg)) {
@@ -267,10 +529,10 @@ extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
           WARNF("Incorrect gep msg: %d vs %d\n", msg.label, gmsg.index_label);
           break;
         }
-        handle_gep(&gmsg, &msg, data->label_info);
+        handle_gep(gmsg, msg);
         break;
       case memcmp_type:
-        info = get_label_info(msg.label, data->label_info);
+        info = get_label_info(msg.label);
         // if both operands are symbolic, no content to be read
         if (info->l1 != CONST_LABEL && info->l2 != CONST_LABEL)
           break;
@@ -297,6 +559,9 @@ extern "C" u32 afl_custom_fuzz_count(my_mutator_t *data, const u8 *buf,
   }
 
   pid = waitpid(pid, NULL, 0);
+
+  // clean up
+  close(pipefds[0]);
 
   return 0;
 
---
+++ driver/aflpp/task.h
@@ -0,0 +1,165 @@
+#pragma once
+
+#include <stdint.h>
+#include <vector>
+#include <map>
+#include <memory>
+#include <unordered_map>
+#include <bitset>
+
+#include "ast.h"
+
+namespace rgd {
+
+// JIT'ed function for each relational constraint
+typedef void(*test_fn_type)(uint64_t*);
+
+// the first two slots of the arguments for reseved for the left and right operands
+static const int RET_OFFSET = 2;
+
+struct Constraint {
+  Constraint(): fn(nullptr), comparison(AstKind::Bool), const_num(0) {}
+
+  // JIT'ed function for a comparison expression
+  test_fn_type fn;
+  // the relational operator
+  rgd::AstKind comparison;
+
+  // During constraint collection, (symbolic) input bytes are recorded
+  // as offsets from the beginning of the input.  However, the JIT'ed
+  // function consumes inputs as an input array.  So, when building the
+  // function, we need to map the offset to the idx in input array,
+  // which is stored in local_map.
+  std::map<size_t, uint32_t> local_map;
+  // if const {false, const value}, if symbolic {true, index in the inputs}
+  // during local search, we use a single global array (to avoid memory
+  // allocation and free) to prepare the inputs, so we need to know where
+  // to load the input values into the input array.
+  std::vector<std::pair<bool, uint64_t>> input_args;
+  // map the offset to iv (initial value)
+  std::unordered_map<uint32_t, uint8_t> inputs;
+  // shape information about the input (e.g., 1, 2, 4, 8 bytes)
+  std::unordered_map<uint32_t, uint32_t> shapes;
+  // number of constant in the input array
+  uint32_t const_num;
+};
+
+struct ConsMeta {
+  // per-constraint arg mapping, so we can share the constraints
+  std::vector<std::pair<bool, uint64_t>> input_args;
+  // input2state inference related
+  bool i2s_feasible;
+  uint64_t op1, op2;
+};
+
+struct SearchTask {
+  SearchTask(): scratch_args(nullptr), max_const_num(0),
+      stopped(false), attempts(0), num_minimal_optima(0),
+      gsol(false), opti_hit(false) {}
+  ~SearchTask() { if (scratch_args) free(scratch_args); }
+
+  uint32_t num_exprs;
+  // constraints, could be shared, strictly read-only
+  std::vector<std::shared_ptr<const Constraint>> constraints;
+  // per-constraint mutable metadata
+  std::vector<std::unique_ptr<ConsMeta>> consmeta;
+
+  // inputs as pairs of <offset (from the beginning of the input, and value>
+  std::vector<std::pair<uint32_t, uint8_t>> inputs;
+  // shape information at each offset
+  std::unordered_map<uint32_t, uint32_t> shapes;
+  // max number of constants in the input array
+  uint32_t max_const_num;
+  // record constraints that use a certain input byte
+  std::unordered_map<uint32_t, std::vector<size_t>> cmap;
+  // the input array used for all JIT'ed functions
+  // all input bytes are extended to 64 bits
+  uint64_t* scratch_args;
+
+  // intermediate states for the search
+  std::vector<uint64_t> orig_distances;
+  std::vector<uint64_t> distances;
+
+  // statistics
+  uint64_t start; //start time
+  bool stopped;
+  int attempts;
+  int num_minimal_optima;
+  bool gsol;
+  bool opti_hit;
+
+  // solutions
+  std::unordered_map<size_t, uint8_t> *rgd_solution;
+  std::unordered_map<size_t, uint8_t> *opti_solution;
+  std::unordered_map<size_t, uint8_t> *hint_solution;
+
+  void finalize() {
+    // aggregate the contraints, map each input byte to a constraint to
+    // an index in the "global" input array (i.e., the scratch_args)
+    std::unordered_map<uint32_t, uint32_t> sym_map;
+    uint32_t gidx = 0;
+    for (size_t i = 0; i < constraints.size(); i++) {
+      std::unique_ptr<ConsMeta> cm = std::make_unique<ConsMeta>();
+      cm->input_args = constraints[i]->input_args;
+      uint32_t last_offset = -1;
+      cm->i2s_feasible = true;
+      for (const auto& [offset, lidx] : constraints[i]->local_map) {
+        auto gitr = sym_map.find(offset);
+        if (gitr == sym_map.end()) {
+          gidx = inputs.size();
+          sym_map[offset] = gidx;
+          inputs.push_back(std::make_pair(offset, constraints[i]->inputs.at(offset)));
+          shapes[offset] = constraints[i]->shapes.at(offset);
+        } else {
+          gidx = gitr->second;
+        }
+        auto slot = cmap.find(gidx);
+        if (slot != cmap.end()) {
+          slot->second.push_back(i);
+        } else {
+          cmap.emplace(std::make_pair(gidx, std::vector<size_t>{i}));
+        }
+        // save the mapping between the local index (i.e., where the JIT'ed
+        // function is going to read the input from) and the global index
+        // (i.e., where the current value corresponding to the input byte
+        // is stored in MutInput)
+        cm->input_args[lidx].second = gidx;
+
+        // check if the input bytes are consecutive
+        // using std::map ensures that the offsets (keys) are sorted
+        if (last_offset != -1 && last_offset + 1 != offset) {
+          cm->i2s_feasible = false;
+        }
+        last_offset = offset;
+      }
+      // FIXME: only support up to 64-bit for now
+      if (constraints[i]->local_map.size() > 8) {
+        cm->i2s_feasible = false;
+      }
+
+      // update the number of required constants in the input array
+      if (max_const_num < constraints[i]->const_num)
+        max_const_num = constraints[i]->const_num;
+
+      // insert the constraint metadata
+      consmeta.push_back(std::move(cm));
+    }
+
+    // allocate the input array, reserver 2 for comparison operands a,b
+    scratch_args = (uint64_t*)aligned_alloc(sizeof(*scratch_args),
+        (2 + inputs.size() + max_const_num + 1) * sizeof(*scratch_args));
+    orig_distances.resize(constraints.size(), 0);
+    distances.resize(constraints.size(), 0);
+  }
+
+  void load_hint() { // load hint
+    for (auto itr = inputs.begin(); itr != inputs.end(); itr++) {
+      auto got = hint_solution->find(itr->first);
+      if (got != hint_solution->end()) 
+        itr->second = got->second; 
+    }
+  }
+
+};
+
+}; // namespace rgd
