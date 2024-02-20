5d97afb7ac375fe270437ab26ec11eeb0f9014d3
Chengyu Song <csong@cs.ucr.edu>
2024-02-20 14:30:44 -0800

    simplify edge cov tracking

--- driver/aflpp/cov.h
+++ driver/aflpp/cov.h
@@ -10,11 +10,29 @@ namespace rgd {
 
 struct BranchContext {
   void *addr;
-  uint32_t id;
   bool direction;
+};
+
+struct HybridBranchContext : public BranchContext {
+  uint32_t id;
+};
+
+struct ContextAwareBranchContext : public BranchContext {
   uint32_t context;
+};
+
+struct LoopAwareBranchContext : public BranchContext {
   uint32_t loop_counter;
+};
+
+struct HistoryAwareBranchContext : public BranchContext {
   uint32_t history;
+};
+
+struct FullBranchContext : public HybridBranchContext,
+                          public ContextAwareBranchContext,
+                          public LoopAwareBranchContext,
+                          public HistoryAwareBranchContext {
 };
 
 class CovManager {
@@ -27,54 +45,33 @@ public:
 };
 
 class EdgeCovManager : public CovManager {
+private:
+  using BranchTargets = std::pair<bool, bool>;
+  std::unordered_map<void*, BranchTargets> branches;
+  std::shared_ptr<BranchContext> _ctx;
+
 public:
+  EdgeCovManager() { _ctx = std::make_shared<BranchContext>(); }
+
   const std::shared_ptr<BranchContext>
   add_branch(void *addr, uint32_t id, bool direction, uint32_t context, bool is_loop_header, bool is_loop_exit) override {
-    auto itr = branches.find(addr);
-    if (itr == branches.end()) {
-      auto ctx = std::make_shared<BranchContext>();
-      ctx->addr = addr;
-      ctx->direction = direction;
-      std::shared_ptr<BranchContext> tt = direction? ctx : nullptr;
-      std::shared_ptr<BranchContext> ft = direction? nullptr : ctx;
-      branches.insert({addr, {tt, ft}});
-      return ctx;
-    } else {
-      if (direction) {
-        auto ctx = itr->second.first;
-        if (!ctx) {
-          ctx = std::make_shared<BranchContext>();
-          ctx->addr = addr;
-          ctx->direction = direction;
-          itr->second.first = ctx;
-        }
-        return ctx;
-      } else {
-        auto ctx = itr->second.second;
-        if (!ctx) {
-          ctx = std::make_shared<BranchContext>();
-          ctx->addr = addr;
-          ctx->direction = direction;
-          itr->second.second = ctx;
-        }
-        return ctx;
-      }
-    }
+    auto &itr = branches[addr];
+    itr.first = direction? true : false;
+    itr.second = direction? false : true;
+    _ctx->addr = addr;
+    _ctx->direction = direction;
+    return _ctx;
   }
 
   bool is_branch_interesting(const std::shared_ptr<BranchContext> context) override {
     auto itr = branches.find(context->addr);
     assert(itr != branches.end());
     if (context->direction) {
-      return itr->second.first == nullptr;
+      return itr->second.first == false;
     } else {
-      return itr->second.second == nullptr;
+      return itr->second.second == false;
     }
   }
-
-private:
-  typedef std::pair<std::shared_ptr<BranchContext>, std::shared_ptr<BranchContext>> BranchTargets;
-  std::unordered_map<void*, BranchTargets> branches;
 };
 
 }; // namespace rgd
