efdb400e01a5c8c6c508b32f633c7ca8049d4419
Haochen Zeng <haochen.zeng10@gmail.com>
2024-03-12 21:58:45 +0000

    regression unit test for stdin input

---
+++ tests/stdin.c
@@ -0,0 +1,41 @@
+// RUN: rm -rf %t.out
+// RUN: mkdir -p %t.out
+// RUN: python -c'print("A"*20)' > %t.bin
+// RUN: clang -o %t.uninstrumented %s
+// RUN: %t.uninstrumented %t.bin < %t.bin | FileCheck --check-prefix=CHECK-ORIG %s
+// RUN: env KO_USE_FASTGEN=1 %ko-clang -o %t.fg %s
+// RUN: env TAINT_OPTIONS="taint_file=%t.bin output_dir=%t.out" %fgtest %t.fg
+// RUN: %t.uninstrumented < %t.out/id-0-0-1 | FileCheck --check-prefix=CHECK-GEN %s
+// RUN: env KO_USE_Z3=1 %ko-clang -o %t.z3 %s
+// RUN: env TAINT_OPTIONS="taint_file=stdin output_dir=%t.out" %t.z3 < %t.bin
+// RUN: %t.uninstrumented < %t.out/id-0-0-1 | FileCheck --check-prefix=CHECK-GEN %s
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib.h"
+
+int main(int argc, char **argv) {
+  char buf[20] = {0};
+  fread(buf, 1, sizeof(buf), stdin);
+
+  uint16_t x = 0;
+  int32_t y = 0;
+  int32_t z = 0;
+  uint32_t a = 0;
+
+  memcpy(&x, buf + 1, 2);  // x 1 - 2
+  memcpy(&y, buf + 4, 4);  // y 4 - 7
+  memcpy(&z, buf + 10, 4); // 10 - 13
+  memcpy(&a, buf + 14, 4); // 14 - 17
+  if (x > 12300 && x < 12350 && z < -100000000 && z > -100000005 &&
+      z != -100000003 && y >= 987654321 && y <= 987654325 && a == 123456789) {
+    // CHECK-GEN: Good
+    printf("Good\n");
+  }
+  else {
+    // CHECK-ORIG: Bad
+    printf("Bad\n");
+  }
+}
