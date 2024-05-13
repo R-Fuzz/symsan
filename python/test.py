4a345028a91152f2cc329cba880732d30c5ab880
Chengyu Song <csong@cs.ucr.edu>
2024-05-13 15:16:47 -0700

    add a python test

---
+++ python/test.py
@@ -0,0 +1,46 @@
+import sys
+import ctypes
+import symsan
+
+class pipe_msg(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [("type", ctypes.c_uint16),
+                ("flags", ctypes.c_uint16),
+                ("instance_id", ctypes.c_uint32),
+                ("addr", ctypes.c_ulonglong),
+                ("context", ctypes.c_uint32),
+                ("id", ctypes.c_uint32),
+                ("label", ctypes.c_uint32),
+                ("result", ctypes.c_uint64)]
+
+prog = sys.argv[1]
+file = sys.argv[2]
+
+symsan.init(sys.argv[1])
+symsan.config(file, args=[prog, file], debug=1, bounds=0)
+symsan.run()
+
+f = open(file, "rb")
+buf = f.read()
+symsan.init_parser([buf])
+
+while True:
+    e = symsan.read_event(ctypes.sizeof(pipe_msg))
+    if len(e) < ctypes.sizeof(pipe_msg):
+        break
+    msg = pipe_msg.from_buffer_copy(e)
+    print(f"received msg: type={msg.type}, flags={msg.flags}, "
+          f"addr={msg.addr:x}, context={msg.context}, cid={msg.id}, "
+          f"label={msg.label}, result={msg.result}")
+
+    tasks = []
+    if msg.type == 0:
+        tasks = symsan.parse_cond(msg.label, msg.result, msg.flags)
+        print(tasks)
+
+    for task in tasks:
+        r, sol = symsan.solve_task(task)
+        print(sol)
+
+symsan.destroy()
+
