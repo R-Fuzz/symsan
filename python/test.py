f8573da33bf6b5324f5c491c1962429659e460b7
Chengyu Song <csong@cs.ucr.edu>
2024-05-23 15:27:35 -0700

    add memcmp recording

--- python/test.py
+++ python/test.py
@@ -13,6 +13,11 @@ class pipe_msg(ctypes.Structure):
                 ("label", ctypes.c_uint32),
                 ("result", ctypes.c_uint64)]
 
+class memcmp_msg(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [("label", ctypes.c_uint32)]
+    content = bytes()
+
 prog = sys.argv[1]
 file = sys.argv[2]
 
@@ -22,7 +27,7 @@ symsan.run()
 
 f = open(file, "rb")
 buf = f.read()
-symsan.init_parser([buf])
+symsan.reset_input([buf])
 
 while True:
     e = symsan.read_event(ctypes.sizeof(pipe_msg))
@@ -37,6 +42,20 @@ while True:
     if msg.type == 0:
         tasks = symsan.parse_cond(msg.label, msg.result, msg.flags)
         print(tasks)
+    elif msg.type == 2 and msg.flags == 1:
+        label = msg.label
+        size = msg.result
+        m = symsan.read_event(ctypes.sizeof(memcmp_msg) + size)
+        if len(m) < ctypes.sizeof(memcmp_msg) + size:
+            print("error reading memcmp msg")
+            break
+        buf = memcmp_msg.from_buffer_copy(m)
+        if buf.label != label:
+            print("error reading memcmp msg")
+            break
+        buf.content = m[ctypes.sizeof(memcmp_msg):]
+        print(f"memcmp content: {buf.content.hex()}")
+        symsan.record_memcmp(label, buf.content)
 
     for task in tasks:
         r, sol = symsan.solve_task(task)
