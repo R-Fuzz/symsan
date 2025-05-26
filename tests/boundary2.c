b43d6158103b6b1b771af5e93105dcc0f963db4d
Chengyu Song <csong@cs.ucr.edu>
2025-05-26 12:51:32 -0700

    update boundary tests

--- tests/boundary2.c
+++ tests/boundary2.c
@@ -1,7 +1,7 @@
 // RUN: rm -rf %t.out
 // RUN: mkdir -p %t.out
 // RUN: python -c"import sys; sys.stdout.buffer.write(b'\x00\x00\x00\x00')" > %t.bin
-// RUN: clang -O1 -g -fsanitize=address -o %t.asan %s
+// RUN: clang -fsanitize=address -o %t.asan %s
 // RUN: env KO_USE_FASTGEN=1 KO_SOLVE_UB=1 %ko-clang -o %t.fg %s
 // RUN: env TAINT_OPTIONS="taint_file=%t.bin output_dir=%t.out solve_ub=1" %fgtest %t.fg %t.bin
 // RUN: not %t.asan %t.out/id-0-0-1 2>&1 | FileCheck %s
@@ -30,6 +30,11 @@ int main (int argc, char** argv) {
   chk_fread(&index, sizeof(index), 1, fp);
   fclose(fp);
 
-  // BUG: out-of-boundary
+  // BUG: off-by-one if index == 26
+  if (index > sizeof(alphabet)) {
+    printf("Bad\n");
+    return 0;
+  }
+
   printf("%c\n", alphabet[index]);
 }
