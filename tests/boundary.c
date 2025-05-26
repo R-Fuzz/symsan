eb3d2a8c42b9a413a500f92765396d3a80cb06c4
Chengyu Song <csong@cs.ucr.edu>
2025-05-26 12:16:55 -0700

    add first bound solving

---
+++ tests/boundary.c
@@ -0,0 +1,35 @@
+// RUN: rm -rf %t.out
+// RUN: mkdir -p %t.out
+// RUN: python -c"import sys; sys.stdout.buffer.write(b'\x00\x00\x00\x00')" > %t.bin
+// RUN: clang -O1 -g -fsanitize=address -o %t.asan %s
+// RUN: env KO_USE_FASTGEN=1 KO_SOLVE_UB=1 %ko-clang -o %t.fg %s
+// RUN: env TAINT_OPTIONS="taint_file=%t.bin output_dir=%t.out solve_ub=1" %fgtest %t.fg %t.bin
+// RUN: not %t.asan %t.out/id-0-0-26 2>&1 | FileCheck %s
+// CHECK: ERROR: AddressSanitizer: stack-buffer-overflow
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib.h"
+
+int main (int argc, char** argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+    return 0;
+  }
+
+  int index = 0;
+  char alphabet[26] = {0};
+
+  for (int i = 0; i < 26; i++) {
+    alphabet[i] = 'A' + i;
+  }
+
+  FILE* fp = chk_fopen(argv[1], "rb");
+  chk_fread(&index, sizeof(index), 1, fp);
+  fclose(fp);
+
+  // BUG: out-of-boundary
+  printf("%c\n", alphabet[index]);
+}
