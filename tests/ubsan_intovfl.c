575f565452f114cb613887d9cc7733fa7532409f
Chengyu Song <csong@cs.ucr.edu>
2025-05-26 15:30:26 -0700

    add intovfl test

---
+++ tests/ubsan_intovfl.c
@@ -0,0 +1,31 @@
+// RUN: rm -rf %t.out
+// RUN: mkdir -p %t.out
+// RUN: python -c"import sys; sys.stdout.buffer.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')" > %t.bin
+// RUN: clang -O0 -fsanitize=address -o %t.asan %s
+// RUN: env KO_DONT_OPTIMIZE=1 KO_USE_FASTGEN=1 KO_SOLVE_UB=1 %ko-clang -o %t.fg %s
+// RUN: env TAINT_OPTIONS="taint_file=%t.bin output_dir=%t.out solve_ub=1" %fgtest %t.fg %t.bin
+// RUN: not %t.asan %t.out/id-0-0-0 2>&1 | FileCheck %s
+// CHECK: ERROR: AddressSanitizer: negative-size-param
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib.h"
+
+int main (int argc, char** argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+    return 0;
+  }
+
+  FILE* fp = chk_fopen(argv[1], "rb");
+  size_t size = 0;
+
+  chk_fread(&size, sizeof(size), 1, fp);
+  fclose(fp);
+
+  char* ptr = malloc(size + 1);
+  memset(ptr, 0, size);
+  free(ptr);
+}
