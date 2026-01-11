250ca74a547ba22f47fd37068bf680febdca4e00
Chengyu Song <csong@cs.ucr.edu>
2026-01-10 17:44:43 -0800

    disable gep_enum for chained string tests

--- tests/memchr_chain.c
+++ tests/memchr_chain.c
@@ -7,7 +7,7 @@
 // First iteration: finds first colon
 // RUN: env TAINT_OPTIONS="taint_file=%t.bin output_dir=%t.out" %fgtest %t.fg %t.bin
 // Second iteration: finds second colon using output from first
-// RUN: env TAINT_OPTIONS="taint_file=%t.out/id-0-0-0 output_dir=%t.out session_id=1" %fgtest %t.fg %t.out/id-0-0-0
+// RUN: env TAINT_OPTIONS="taint_file=%t.out/id-0-0-0 output_dir=%t.out session_id=1 enum_gep=0" %fgtest %t.fg %t.out/id-0-0-0
 // RUN: %t.uninstrumented %t.out/id-0-1-1 | FileCheck --check-prefix=CHECK-GEN %s
 
 // Test chained memchr with bounded length from previous result:
